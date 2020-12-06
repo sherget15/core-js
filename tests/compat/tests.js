c9f6ceea1fc5ec8e15cf171325cbe5b4cbda1350
Denis Pushkarev <zloirock@zloirock.ru>
2020-12-06 22:00:36 +0700

    drop `esnext.math.seeded-prng`

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1313,9 +1313,6 @@ GLOBAL.tests = {
   'esnext.math.scale': function () {
     return Math.scale;
   },
-  'esnext.math.seeded-prng': function () {
-    return Math.seededPRNG;
-  },
   'esnext.math.signbit': function () {
     return Math.signbit;
   },
