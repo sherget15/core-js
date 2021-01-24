02fce3f76d0d42417e9b4a531dda9de3b7f68ae6
Denis Pushkarev <zloirock@zloirock.ru>
2021-01-24 18:08:57 +0700

    split `es.string.trim-{ end, start }` to `es.string.trim-{ end, left, right, start }`

--- packages/core-js-compat/src/data.js
+++ packages/core-js-compat/src/data.js
@@ -933,6 +933,16 @@ const data = {
     firefox: '61',
     safari: '12.1',
   },
+  'es.string.trim-left': {
+    chrome: '66',
+    firefox: '61',
+    safari: '12.0',
+  },
+  'es.string.trim-right': {
+    chrome: '66',
+    firefox: '61',
+    safari: '12.1',
+  },
   'es.string.trim-start': {
     chrome: '66',
     firefox: '61',
